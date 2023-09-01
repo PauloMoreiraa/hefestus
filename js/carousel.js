91cbee3072175c637c3fc0e8eb4c3b47edd59f45
Paulo Moreira <56117238+PauloMoreiraa@users.noreply.github.com>
2023-09-01 18:17:05 -0300

    categorias atualizadas

---
+++ js/carousel.js
@@ -0,0 +1,28 @@
+
+  $(document).ready(function(){
+    $('.tecnologias').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      nextArrow: false,
+      prevArrow: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
